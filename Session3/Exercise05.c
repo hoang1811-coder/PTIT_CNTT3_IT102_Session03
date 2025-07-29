66e1b649f5f3f4b7e796849553c19c4430f2c168
hoang1811-coder <vhoangssz1@gmail.com>
2025-07-29 23:52:46 +0700

    Add files via upload

--- Session3/Exercise05.c
+++ Session3/Exercise05.c
@@ -1,19 +1,48 @@
-#include<stdio.h>
-int main() {
-    int Number1, Number2, Number3;
-    printf("Nhap so thu nhat :");
-    scanf("%d", &Number1);
-    printf("Nhap so thu hai : ");
-    scanf("%d", &Number2);
-    printf("Nhap so thu ba : ");
-    scanf("%d", &Number3);
+#include <stdio.h>
 
-    int Number;
-    if ((Number1 + Number2) / 2 == Number3){
-        printf("So thu 3 nam trong giua so 1 va 2");
-    }else {
-        printf("So thu 3 khong nam trong giua so 1 va 2");
+
+struct SinhVien {
+    int STT;
+    char HoTen[50];
+    int Tuoi;
+    char SoDT[15];
+    char Email[30];
+};
+
+void inTieuDe() {
+    printf("\nDANH SACH SINH VIEN\n");
+    printf("|-----|-----------------|------|---------------|--------------------------|\n");
+    printf("| STT | Ho va ten       | Tuoi | So dien thoai | Email                    |\n");
+    printf("|-----|-----------------|------|---------------|--------------------------|\n");
+}
+
+void inSinhVien(struct SinhVien sv) {
+    printf("| %-3d | %-15s | %4d | %-13s | %-24s |\n",
+           sv.STT, sv.HoTen, sv.Tuoi, sv.SoDT, sv.Email);
+}
+
+int main() {
+    struct SinhVien danhSach[] = {
+        {1, "Nguyen Van A", 20, "0904488481", "anv@rikkeiacademy.com"},
+        {2, "Nguyen Van B", 21, "0904488482", "bnv@rikkeiacademy.com"},
+        {3, "Nguyen Van C", 18, "0904488483", "cnv@rikkeiacademy.com"},
+        {4, "Nguyen Van D", 19, "0904488484", "dnv@rikkeiacademy.com"},
+        {5, "Nguyen Van E", 22, "0904488485", "env@rikkeiacademy.com"},
+        {6, "Nguyen Van F", 21, "0904488486", "fnv@rikkeiacademy.com"},
+        {7, "Nguyen Van G", 23, "0904488487", "gnv@rikkeiacademy.com"},
+        {8, "Nguyen Van H", 19, "0904488488", "hnv@rikkeiacademy.com"},
+        {9, "Nguyen Van I", 18, "0904488489", "inv@rikkeiacademy.com"},
+        {10, "Nguyen Van K", 21, "0904488480", "knv@rikkeiacademy.com"}
+    };
+
+    int soLuong = sizeof(danhSach)/sizeof(danhSach[0]);
+
+    inTieuDe();
+
+    for(int i = 0; i < soLuong; i++) {
+        inSinhVien(danhSach[i]);
     }
+    printf("|-----|-----------------|------|---------------|--------------------------|\n");
 
     return 0;
 }
